cf86cda0a0a5133c499c6979f5a0d056f795e5c2
Hamzeh Atasheneh <hamzeh.atasheneh@oakam.com>
2018-04-15 22:53:52 +0100

    fin

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-// import Home from './Components/Home';
+import Home from './Components/Home';
 import ApplicationForm from './Components/ApplicationForm/ApplicationForm';
-// import ApplicationForm from './Components/ApplicationForm/ApplicationForm';
-// import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ApplicationForm/>
-      {/* <Home/> */}
+        
+        <ApplicationForm/> 
       </div>
     );
   }
 }
-
 export default App;
